9a13637db3735193028ebdfbd7bac84b756b4207
Igor Nabokov <inabokov@65apps.com>
2018-01-28 14:53:05 +0400

    Initial Commit

---
+++ StackoverflowReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StackoverflowReader
+//
+//  Created by Igor Nabokov on 28/01/2018.
+//  Copyright © 2018 Igor Nabokov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StackoverflowReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StackoverflowReader
+//
+//  Created by Igor Nabokov on 28/01/2018.
+//  Copyright © 2018 Igor Nabokov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
