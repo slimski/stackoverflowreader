fc1307e3112aa63a13015ae7ea1ba441d790b440
Igor Nabokov <inabokov@65apps.com>
2018-02-01 12:40:38 +0400

    constants

---
+++ StackoverflowReader/Constants.h
@@ -0,0 +1,9 @@
+//
+// Created by Igor Nabokov on 01/02/2018.
+// Copyright (c) 2018 Igor Nabokov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString *const baseUrl;
+extern NSString *const searchUrl;
---
+++ StackoverflowReader/Constants.m
@@ -0,0 +1,11 @@
+//
+// Created by Igor Nabokov on 01/02/2018.
+// Copyright (c) 2018 Igor Nabokov. All rights reserved.
+//
+
+#import "Constants.h"
+
+
+NSString *const baseUrl = @"https://api.stackexchange.com/2.2/";
+NSString *const searchUrl = @"search?order=desc&sort=activity&intitle=%@&site=stackoverflow";
+
